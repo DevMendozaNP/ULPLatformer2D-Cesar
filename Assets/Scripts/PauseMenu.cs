460884f7d12883e9f244e7a5542293bbe21d4742
DevMendozaNP <cesmend.799@gmail.com>
2023-09-27 18:27:25 -0500

    Final Build
    Incluye el build de WebGL presentado en clase

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
